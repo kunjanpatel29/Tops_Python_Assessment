e3b92eccb6fd7bd2cb36ff15c99c9de99bf157e5
kunjanpatel29 <patelkunjan1319@gmail.com>
2023-05-18 00:17:57 +0530

    18-05-2023

--- Core_Python_Assessment.py
+++ Core_Python_Assessment.py
@@ -1,4 +1,4 @@
-# # Write a program to demonstrate the bank management Console based application.
+# Write a program to demonstrate the bank management Console based application.
 
 # Create Bank Class
 class Bank:
@@ -11,9 +11,9 @@ class Bank:
 
 # Create Account Class
 class Account:
-    def __init__(self, number, owner, balance):
+    def __init__(self, number, cname, balance):
         self.number = number
-        self.owner = owner
+        self.cname = cname
         self.balance = balance
         self.transactions = []
 
@@ -37,9 +37,10 @@ class Transaction:
         self.amount = amount
         self.type = type
 
-# Main Class 
+# Main Function 
 def main():
     print("WELCOME TO PYTHON BANK MANAGEMENT SYSTEM\n")
+
     bank_name = input('Enter Bank Name: ')
     bank = Bank(bank_name)
     print('\nBank', bank_name, 'Created Successfully')
@@ -49,14 +50,17 @@ def main():
         print('\t\t 3.Withdraw')
         print('\t\t 4.Check Balance')
         print('\t\t 5.Exit')
+
         choice = int(input('Enter your choice(1-5): '))
+
         if choice == 1:
             ac_no = input('Enter account number: ')
-            acc_owner = input('Enter account owner name: ')
-            acc_balance = float(input('Enter opening balance: '))
-            account = Account(ac_no, acc_owner, acc_balance)
+            cname = input('Enter Customer name: ')
+            balance = float(input('Enter opening balance: '))
+            account = Account(ac_no, cname, balance)
             bank.add_account(account)
             print('\nAccount created successfully')
+
         elif choice == 2:
             ac_no = input('Enter account number: ')
             amount = float(input('Enter amount to deposit: '))
@@ -66,6 +70,7 @@ def main():
                 print('Deposit successful')
             else:
                 print('Account not found')
+
         elif choice == 3:
             ac_no = input('Enter account number: ')
             amount = float(input('Enter amount to withdraw: '))
@@ -75,11 +80,14 @@ def main():
                 print('Withdrawal successful')
             else:
                 print('Account not found')
+
         elif choice == 4:
-            account.check_balance(acc_balance)
+            account.check_balance(balance)
+
         elif choice == 5:
             print('Thank you for using the Bank Management System')
             break
+
         else:
             print('Invalid choice')
 
